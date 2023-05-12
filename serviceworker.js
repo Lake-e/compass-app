bb348638732220278b875ff92faecbbf2c56dd24
Lake <95671554+Lake-e@users.noreply.github.com>
2023-05-12 11:50:22 -0400

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,112 +1,41 @@
 /*
-Copyright 2015, 2019 Google Inc. All Rights Reserved.
+Copyright 2021 Google LLC
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
+      http://www.apache.org/licenses/LICENSE-2.0
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
-*/
+ */
 
-// Incrementing OFFLINE_VERSION will kick off the install event and force
-// previously cached resources to be updated from the network.
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = 'offline';
-// Customize this with a different URL if needed.
-const OFFLINE_URL = '/compass-app/index.html';
+// Choose a cache name
+const cacheName = 'cache-v1';
+// List the files to precache
+const precacheResources = ['/', '/index.html', '/compass-app/compass-dial.jpeg'];
 
-var cacheAssets = [
-    '/compass-app/resources/compass-app-icon.png',
-    '/compass-app/compass-dial.jpeg',
-    '/compass-app/index.html'
-]
-
+// When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    // Setting {cache: 'reload'} in the new request will ensure that the response
-    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-    //await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
-    cache.addAll(cacheAssets)
-                 // When everything is set
-                 .then(() => self.skipWaiting())
-  })());
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    // Enable navigation preload if it's supported.
-    // See https://developers.google.com/web/updates/2017/02/navigation-preload
-    if ('navigationPreload' in self.registration) {
-      await self.registration.navigationPreload.enable();
-    }
-  })());
-
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
+  console.log('Service worker activate event!');
 });
 
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
 self.addEventListener('fetch', (event) => {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        // First, try to use the navigation preload response if it's supported.
-        const preloadResponse = await event.preloadResponse;
-        if (preloadResponse) {
-          return preloadResponse;
-        }
-
-        const networkResponse = await fetch(event.request);
-        return networkResponse;
-      } catch (error) {
-        // catch is only triggered if an exception is thrown, which is likely
-        // due to a network error.
-        // If fetch() returns a valid HTTP response with a response code in
-        // the 4xx or 5xx range, the catch() will NOT be called.
-        console.log('Fetch failed; returning offline page instead.', error);
-
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(event.request);
-        return cachedResponse;
-      }
-    })());
-  }
-
-  // If our if() condition is false, then this fetch handler won't intercept the
-  // request. If there are any other fetch handlers registered, they will get a
-  // chance to call event.respondWith(). If no fetch handlers call
-  // event.respondWith(), the request will be handled by the browser as if there
-  // were no service worker involvement.
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }),
+  );
 });
-
-/*
-self.addEventListener('fetch', (event) => {
-  event.respondWith((async() => {
-
-    const cache = await caches.open(CACHE_NAME);
-
-    try {
-        const cachedResponse = await cache.match(event.request);
-        if(cachedResponse) {
-            console.log('cachedResponse: ', event.request.url);
-            return cachedResponse;
-        }
-
-        const fetchResponse = await fetch(event.request, {mode:'no-cors'});
-        if(fetchResponse) {
-            console.log('fetchResponse: ', event.request.url);
-            await cache.put(event.request, fetchResponse.clone());
-            return fetchResponse;
-        }
-    }   catch (error) {
-        console.log('Fetch failed: ', error);
-        const cachedResponse = await cache.match('/en/offline.html');
-        return cachedResponse;
-    }
-  })());
-});*/
